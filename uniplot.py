f55fa8e4d0f012c4a119236104d927a991c9415b
b9003599 <b9003599@newcastle.ac.uk>
2019-10-14 11:26:46 +0100

    Reading UniProt XML

--- uniplot.py
+++ uniplot.py
@@ -1,4 +1,8 @@
 import gzip
-for l in gzip.open("uniprot_receptor.xml.gz"):
-    print(l.decode().strip())
+from Bio import SeqIO
+
+handle = gzip.open("uniprot_receptor.xml.gz")
+
+for record in SeqIO.parse(handle, "uniprot-xml"):
+    print(record)
 
