b0031e3ebeb82f60cd8e8d528a937dafcdb68d68
b9003599 <b9003599@newcastle.ac.uk>
2019-10-23 00:32:31 +0100

    Depth 2 plotting in progress

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -8,6 +8,6 @@ def average_len(records):
 def average_len_taxa(records):
     record_by_taxa = {}
     for r in records:
-        taxa = r.annotations["taxonomy"][0]
+        taxa = r.annotations["taxonomy"][1]
         record_by_taxa.setdefault(taxa, []).append(r)
     return {taxa:average_len(record) for (taxa, record) in record_by_taxa.items()}
