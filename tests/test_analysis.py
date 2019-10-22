9e01db35498ad58ef1ef2fde018c630eb52e2b50
b9003599 <b9003599@newcastle.ac.uk>
2019-10-22 06:22:31 +0100

    Adding Test and Continuous Integration

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,4 +1,8 @@
 import pytest
+import uniplot.analysis
+import uniplot.parse
 
-def test_hello_world():
-    assert True
+test_file = "resources/uniprot_sprot_small.xml.gz"
+
+def test_average():
+    assert uniplot.analysis.average_len(uniplot.parse.uniprot_seqrecords(test_file)) == 5449/18
