3b1ae88cb9db32be70e4a31a28e26431bddc8148
b9003599 <b9003599@newcastle.ac.uk>
2019-10-28 11:33:58 +0000

    Setting up help options Documentation: Extended (WIP) Eye Candy and Help information: Setting up easy to read help (WIP) V1.2.0

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -35,23 +35,23 @@ def loc(args, loco = None):
 
 def cli():
     #Create new parser
-    parser = argparse.ArgumentParser(prog="uniplot")
+    parser = argparse.ArgumentParser(description="This program analyses and returns GNU zipped uniprot-xml database files")
     parser.add_argument("--loco", type=str, default=None)
     #Add subparsers
     subparsers = parser.add_subparsers(help="Sub Command Help")
 
-    f_location = subparsers.add_parser('location')
-    f_location.add_argument('location', type=str)
+    f_location = subparsers.add_parser('location', help="Use 'location' to save the file location that you want to run other commands on")
+    f_location.add_argument('location', type=str,metavar='file_location', help='Type your file location in place of "file location"')
     f_location.set_defaults(func=loco)
-    subparsers.add_parser("dump").set_defaults(run=loc, func=dump)
-    subparsers.add_parser("list").set_defaults(run=loc, func=names)
-    subparsers.add_parser("average").set_defaults(run=loc, func=average)
-    taxanom = subparsers.add_parser("plot-taxa")
-    taxanom.add_argument("--depth", type=int, default=0)
-    taxanom.add_argument("--pie", type=int, default=0)
+    subparsers.add_parser("dump", help="Use 'dump' to print all the records from the file").set_defaults(run=loc, func=dump)
+    subparsers.add_parser("list", help="Use 'list' to print all the protein sequence names from the file").set_defaults(run=loc, func=names)
+    subparsers.add_parser("average", help="Use 'average' to obtain the average length of proteins from all records of the file").set_defaults(run=loc, func=average)
+    taxanom = subparsers.add_parser("plot-taxa", help='Plot the average lengths of proteins distributed by all categories at a taxonomy level as a bar graph or pie chart. Use help argument(-h) with plot-taxa for more detail')
+    taxanom.add_argument("--depth", type=int, default=0,metavar="<Level number>", help='Use this optional argument to supply the level of taxonomy you want to access (1 is the highest and is the default value)')
+    taxanom.add_argument("--piemode", type=int, default=0,metavar=" [1 or 0] ", help='If you want to see the average graph in the form of a pie chart, pass the value 1 with this argument(1 = True and default is false)')
     taxanom.set_defaults(run=loc, func=plot_average_by_taxa)
-    numpie = subparsers.add_parser("pie")
-    numpie.add_argument("--depth", type=int, default=0)
+    numpie = subparsers.add_parser("pie", help="Give the number of records sorted by taxanomic categories in the form of a pie chart.(depth argument can be passed to change taxanomy level. Use '-h' with pie for more information")
+    numpie.add_argument("--depth", type=int, default=0, help='Use this optional argument to supply the level of taxonomy you want to access (1 is the highest and is the default value)')
     numpie.set_defaults(run=loc, func=npie)
 
     #Parse the command line
